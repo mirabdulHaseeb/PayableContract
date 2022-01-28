816149ff233c2038f73ab7b5cb425f3239ad0044
mirabdulHaseeb <abdulhaseeb732@gmail.com>
2022-01-28 08:06:28 +0500

    AggregatorV3 Interface

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -2,10 +2,16 @@
 
 pragma solidity >=0.6.0 <0.9.0;
 
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface";
+
 contract FundMe {
     mapping(address => uint256) public addressToAmountFunded;
 
     function fund() public payable {
         addressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    function getVersion() public view returns(uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
     }
 }
