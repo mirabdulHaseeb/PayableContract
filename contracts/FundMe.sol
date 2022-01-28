d6e38b3b7a69a98c2ab7a6f0deb31dfc608a27c2
mirabdulHaseeb <abdulhaseeb732@gmail.com>
2022-01-28 23:46:27 +0500

    Eth-USD conversion

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,17 +1,45 @@
 //SPDX-License-Identifier: MIT
 
-pragma solidity >=0.6.0 <0.9.0;
+pragma solidity ^0.8.0;
 
-import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
+
+    address public owner;
     mapping(address => uint256) public addressToAmountFunded;
 
+    constructor() public {
+        owner = msg.sender;
+    }
+
     function fund() public payable {
+        uint256 minUSD = 50 * 1e18;
+        require(getConversionRate(msg.value) >= minUSD, "Not enough funds");
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
     function getVersion() public view returns(uint256) {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        return priceFeed.version();
+    }
+
+    function getPrice() public view returns(uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        (,int256 answer,,,) = priceFeed.latestRoundData();
+        // answer returns 10^8
+        // wei statndard is 10^18 i.e. 1 eth = 10^18 wei
+        return uint256(answer * 1e10);
+    }
+
+    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+
+    function withdraw() payable public {
+        require(msg.sender == owner, "Only the owner can withdraw");
+        payable(msg.sender).transfer(address(this).balance);
     }
 }
