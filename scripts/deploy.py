6bdd60d9cd334898e11d16223fd99ff3652248d5
mirabdulHaseeb <abdulhaseeb732@gmail.com>
2022-02-02 00:06:51 +0500

    deployment restructuring

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,14 +1,9 @@
 from brownie import FundMe, accounts, network, config
+from scripts.helpful_scripts import get_account
 
 def deploy_fundme():
     owner = get_account()
     FundMe.deploy({"from": owner})
-
-def get_account():
-    if network.show_active() == "development":
-        return accounts[0]
-    else:
-        return accounts.add(config["wallets"]["from_key"])
 
 def main():
     deploy_fundme()
