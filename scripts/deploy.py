fe9fe7f4e2a1b0fcb8eecd2bb6046a696cbe283b
mirabdulHaseeb <abdulhaseeb732@gmail.com>
2022-02-01 14:29:30 +0500

    get_account function created

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,8 +1,14 @@
-from brownie import FundMe, accounts
+from brownie import FundMe, accounts, network, config
 
 def deploy_fundme():
-    owner = accounts[0]
+    owner = get_account()
     FundMe.deploy({"from": owner})
+
+def get_account():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
 
 def main():
     deploy_fundme()
