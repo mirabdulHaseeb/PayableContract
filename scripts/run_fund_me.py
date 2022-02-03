49898c936937871d508940854ed4bfbce6285f69
mirabdulHaseeb <abdulhaseeb732@gmail.com>
2022-02-03 13:18:14 +0500

    fund and withdraw

--- scripts/run_fund_me.py
+++ scripts/run_fund_me.py
@@ -6,6 +6,15 @@ def run_fund_me():
     fund_me = FundMe[-1]
     print(f"Version: {fund_me.getVersion()}")
     print(f"Eth Price (per dollar): {fund_me.getConversionRate(1)}")
+    print(f"Balance: {fund_me.balance()}")
+    amount_funded = 50000000000000000
+    print(f"Funding {amount_funded} ...")
+    fund_me.fund({"from": account, "value": amount_funded})
+    print(f"Balance: {fund_me.balance()}")
+    print("Withdraw funds ...")
+    fund_me.withdraw({"from": account})
+    print(f"Balance: {fund_me.balance()}")
+
 
 def main():
     run_fund_me()
