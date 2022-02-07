da073f6e7982090d8e7b0cb08a76954cf9afb6bd
mirabdulHaseeb <abdulhaseeb732@gmail.com>
2022-02-07 23:42:25 +0500

    DECIMALS and STARTING_PRICE values changed

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -3,8 +3,8 @@ from web3 import Web3
 
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
 
-DECIMALS = 18
-STARTING_PRICE = 2000
+DECIMALS = 8
+STARTING_PRICE = 2000 * 1e8
 
 def get_account():
     if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
@@ -16,5 +16,5 @@ def deploy_mocks():
     print(f"The active network is {network.show_active()}")
     print("Deploying Mocks...")
     if len(MockV3Aggregator) == 0:
-        MockV3Aggregator.deploy(DECIMALS, Web3.toWei(STARTING_PRICE, "ether"), {"from": get_account()})
+        MockV3Aggregator.deploy(DECIMALS, STARTING_PRICE, {"from": get_account()})
     print("Mocks deployed!")
